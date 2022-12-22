a15fb7a45451eae588334f3913124b07cc782700
Choconok <102304870+Choconok@users.noreply.github.com>
2022-12-22 11:41:57 +0900

    Update multiply.c

--- src/multiply.c
+++ src/multiply.c
@@ -17,33 +17,40 @@ sign = (num1->d == num2->d); //'+'=true, '-'=false
 // 소수점 위치
 unsigned long long point1 = 0;
 unsigned long long point2 = 0;
+
 num_copy = last_link(num1); //마지막에서 탐색 시작
-for (; num_copy->d != '.'; num_copy = num_copy->prev)
-point1++;
+while(num_copy->d != '.'){
+    num_copy = num_copy->prev;
+    point1++;
+}
 
 num_copy = last_link(num2);
-for (; num_copy->d != '.'; num_copy = num_copy->prev)
-point2++;
-
-point = point1 + point2;
+while(num_copy->d != '.'){
+    num_copy = num_copy->prev;
+    point2++;
+}
+point = point1 + point2; //최종 소수점 위치 구함
 
 // 점 빼기
 num_copy = next_num1;
-for (; num_copy->d != '.'; num_copy = num_copy->next);
+while(num_copy->d != '.')
+    num_copy->next;
 del_link(num_copy);
 
 num_copy = next_num2;
-for (; num_copy->d != '.'; num_copy = num_copy->next);
+while(num_copy->d != '.'){
+    num_copy->next;
 del_link(num_copy);
 
-// 계산
+
+// 계산 (손계산 원리 참조)
 cal1 = last_link(next_num1); //다시 역으로 만들어 처음처럼 만들어준다
 unsigned long long int zero = 0; 
 for (; cal1 != NULL; cal1 = cal1->prev) {
     cal2 = last_link(next_num2);
     char down = (cal1->d - '0') * (cal2->d - '0'); 
     char up = down / 10; //올림수
-    down % 10 = down; 
+    down % 10 = down; //나머지 그대로
 
     input = char_to_list(down + '0');
 
@@ -86,7 +93,8 @@ for (; cal1 != NULL; cal1 = cal1->prev) {
     }
 
     //곱하기 결과들 덧셈
-    if (!zero) answer = copy_link(input);
+    if (!zero) 
+        answer = copy_link(input);
 
     else {
         unsigned long long len1 = count(answer);
